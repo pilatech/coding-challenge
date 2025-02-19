a4481b647e8a7cfae9f2c7dbb7b10bbc2c308ce4
Pilate <pilatech6@gmail.com>
2025-02-19 06:25:26 +0200

    live form with error as default status

--- React/0x0-live-form/src/Form.jsx
+++ React/0x0-live-form/src/Form.jsx
@@ -4,25 +4,33 @@ import Input from './Input'
 export default function Form() {
  const [formData, setFormData] = useState({
     username: {
-        value: '',
-        error: false
+        value: ''
     },
     email: {
-        value: '',
-        error: false
+        value: ''
     },
     password: {
-        value: '',
-        error: false
+        value: ''
     }
  })
+
+ const [formError, setFormError] = useState(true);
+
+ function handleChange(event){
+    const newFormData = {}
+   for (const field in formData){
+        newFormData[field] = {...formData[field]};
+   }
+   newFormData[event.target.name].value = event.target.value
+   setFormData(newFormData);
+ }
   return (
     <form className="form">
         <h2 className="form__title">Sign up for a free account</h2>
         { Object.keys(formData).map(name => (
-            <Input name={name} data={formData[name]}/>
+            <Input name={name} data={formData[name]} handleChange={handleChange} setFormError={setFormError}/>
         ))}
-        <button className="button">Submit</button>
+        <button className="button" disabled={formError}>Submit</button>
     </form>
   )
 }
--- React/0x0-live-form/src/Input.jsx
+++ React/0x0-live-form/src/Input.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
-export default function Input({ data, name }) {
+export default function Input({ data, name, handleChange, setFormError }) {
+  const inputType = name == 'password' ? 'password' : 'text';
+  const validEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  let errorMsg = null;
+  if (!data.value && name !== 'email') {
+    errorMsg = <p className="input__error">Field cannot be empty</p>;
+    setFormError(true);
+  } else if (name === 'email' && !validEmail(data.value)) {
+    errorMsg = <p className="input__error">Invalid Email Address</p>
+  } else if((name === 'email' && validEmail(data.value)) || (data.value && name !== 'email')){
+    setFormError(false);
+  }
+
   return (
     <div className="input__control">
         <label className="input__label" htmlFor="name">{name[0].toUpperCase() + name.slice(1)}</label>
-        { data.error && <p className="input__error">Something is not right</p> }
-        <input className="input" type="text" id="name"/>
+        { errorMsg }
+        <input 
+        className="input" 
+        onChange={handleChange} 
+        type={inputType} 
+        id="name"
+        name={name}
+        value={data.value}
+        />
     </div>
   )
 }
